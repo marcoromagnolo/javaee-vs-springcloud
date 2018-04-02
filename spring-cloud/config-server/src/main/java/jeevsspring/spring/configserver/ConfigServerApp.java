32c5064fcf730b140062232919fd685de0af37c1
Marco Romagnolo <marco@romagnolo.me>
2018-04-02 02:23:08 +0200

    Fix Config

--- spring-cloud/config-server/src/main/java/jeevsspring/spring/configserver/ConfigServerApp.java
+++ spring-cloud/config-server/src/main/java/jeevsspring/spring/configserver/ConfigServerApp.java
@@ -2,6 +2,7 @@ package jeevsspring.spring.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
